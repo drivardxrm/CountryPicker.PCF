87fa82b99d00270c162ef2dc22048042edd3166b
David Rivard <38399134+drivardxrm@users.noreply.github.com>
2025-10-18 10:44:45 -0400

    storybook

--- stories/CountryPicker.stories.tsx
+++ stories/CountryPicker.stories.tsx
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import { fn } from 'storybook/test';
 
-//import { Button } from './Button';
 import CountryPickerApp from '../CountryPicker/components/CountryPickerApp';
 import React from 'react';
 
@@ -12,7 +11,7 @@ const meta = {
   component: CountryPickerApp,
   decorators: [
     (Story) => (
-      <div style={{ margin: '3em' , maxWidth:'350px'}}>
+      <div style={{ margin: '3em' , maxWidth:'500px'}}>
         {Story()}
       </div>
     )
@@ -21,6 +20,9 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
+    controls:{
+        exclude: ['instanceid','onChange','displayinfo', 'promoted', 'limit', 'countrycode' ]
+    }
   },
   
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
@@ -55,19 +57,111 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    instanceid: 'storybook-instance',
-    isDarkMode: false,
-    readonly: false,
-    masked: false,
-    promoted:undefined,
-    limit: undefined,
-    countrycode: '',
-    language : 'en',
-    displayinfo: true,
-    onChange: (countrycode:string, countryname:string) => {
-      console.log(`PCF NotifyOutputChanged  => ${countrycode}:${countryname}`)
-    },
+    
   },
 };
+
+export const Selected: Story = {
+  args: {
+    countrycode: 'CAN',
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Disabled: Story = {
+  args: {
+    readonly: true,
+  },
+};
+
+export const DisabledSelected: Story = {
+  args: {
+    readonly: true,
+    countrycode: 'CAN',
+  },
+};
+
+export const Masked: Story = {
+  args: {
+    masked: true,
+  },
+};
+
+export const Limit: Story = {
+  args: {
+    limit: ['USA','CAN','MEX'],
+  },
+};
+
+export const Promoted: Story = {
+  args: {
+    promoted:['USA','CAN','MEX'],
+  },
+};
+
+export const French: Story = {
+  args: {
+    countrycode: 'FRA',
+    language : 'fr',
+   
+  },
+};
+
+export const German: Story = {
+  args: {
+    countrycode: 'DEU',
+    language : 'de',
+   
+  },
+};
+
+export const Spanish: Story = {
+  args: {
+    countrycode: 'ESP',
+    language : 'es',
+   
+  },
+};
+
+export const Persian: Story = {
+  args: {
+    countrycode: 'IRN',
+    language : "fa",
+   
+  },
+};
+
+export const Italian: Story = {
+  args: {
+    countrycode: 'ITA',
+    language : 'it',
+   
+  },
+};
+
+export const Japanese: Story = {
+  args: {
+    countrycode: 'JPN',
+    language : 'ja',
+   
+  },
+};
+
+export const Dutch: Story = {
+  args: {
+    countrycode: 'NLD',
+    language : 'nl',
+   
+  },
+};
+
+export const Portugese: Story = {
+  args: {
+    countrycode: 'PRT',
+    language : 'pt',
+   
+  },
+};
+
 
 
